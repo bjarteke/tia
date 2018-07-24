fdd3b9012828942ef5e1b630e045f620f4ee4d0f
Stian Westerhus Vale <stvale@Stians-MacBook-Pro-2.local>
2018-07-24 09:16:21 +0200

    notifications up and running:))

--- src/providers/location-tracker/location-tracker.ts
+++ src/providers/location-tracker/location-tracker.ts
@@ -42,13 +42,6 @@ export class LocationTracker {
       this.lng = location.longitude;
       this.lastTimestamp = location.time;
       this.insidePolygonCheck(this.lat, this.lng);
-      console.log('Her kommer logg! \n');
-      console.log(this.paJobb)
-      console.log(this.sentNotification)
-      if(this.paJobb && this.sentNotification == false){
-        this.notifications.scheduleNotification();
-        this.sentNotification = true;
-      }
     });
  
   }, (err) => {
@@ -101,6 +94,18 @@ this.watch = this.geolocation.watchPosition(options).filter((p: any) => p.code =
       console.log("ER VI PÅ JOBB?????")
       console.log(booleanPointInPolygon(pt,poly));
       this.paJobb = booleanPointInPolygon(pt,poly);
+      this.evaluateSendNotification()
+  }
+
+  evaluateSendNotification(){
+    console.log('Her kommer logg! \n');
+    console.log(this.paJobb)
+    console.log(this.sentNotification)
+    if(this.paJobb && this.sentNotification == false){
+      this.notifications.scheduleNotification();
+      this.sentNotification = true;
+    }
+
   }
 
 
