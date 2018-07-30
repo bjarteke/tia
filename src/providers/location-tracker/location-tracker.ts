3498040fa0cab4df1c72d635ce5489808b850326
Stian Westerhus Vale <stvale@Stians-MacBook-Pro-2.local>
2018-07-30 16:34:13 +0200

    added automatic check in logic

--- src/providers/location-tracker/location-tracker.ts
+++ src/providers/location-tracker/location-tracker.ts
@@ -6,6 +6,7 @@ import booleanPointInPolygon from '@turf/boolean-point-in-polygon';
 import turf from 'turf'; 
 import { NotificationsProvider } from '../notifications/notifications';
 import { Observable } from 'rxjs';
+import { FirebaseServiceProvider } from '../firebase-service/firebase-service';
 
 
 @Injectable()
@@ -17,12 +18,13 @@ export class LocationTracker {
   public paJobb : boolean = false;
   public changed: boolean = false; 
   public sentNotification: boolean = false;
+  public hasArrived: boolean = false;
   public lastTimestamp : any;
   public onLocationTime;
   public forlotTid = null; 
   public initialRun = false;
  
-  constructor(public zone: NgZone, public backgroundGeolocation: BackgroundGeolocation, private geolocation : Geolocation, public notifications: NotificationsProvider) {
+  constructor(public zone: NgZone, public backgroundGeolocation: BackgroundGeolocation, private geolocation : Geolocation, public notifications: NotificationsProvider, public fsp: FirebaseServiceProvider) {
  
   }
  
@@ -39,7 +41,7 @@ export class LocationTracker {
  
   this.backgroundGeolocation.configure(config).subscribe((location) => {
  
-    console.log('BackgroundGeolocation:  ' + location.latitude + ',' + location.longitude);
+    //console.log('BackgroundGeolocation:  ' + location.latitude + ',' + location.longitude);
  
     // Run update inside of Angular's zone
     this.zone.run(() => {
@@ -51,7 +53,7 @@ export class LocationTracker {
  
   }, (err) => {
  
-    console.log(err);
+    //console.log(err);
  
   });
  
@@ -66,13 +68,31 @@ let options = {
  
 this.watch = this.geolocation.watchPosition(options).filter((p: any) => p.code === undefined).subscribe((position: Geoposition) => {
  
-  console.log(position);
+  //console.log(position);
  
   // Run update inside of Angular's zone
   this.zone.run(() => {
     this.lat = position.coords.latitude;
     this.lng = position.coords.longitude;
     this.insidePolygonCheck(this.lat, this.lng);
+    var now = new Date();
+    if (this.paJobb && this.fsp.isWorking(now)){
+      console.log('her er gebben vettu');
+      
+      if (!this.hasArrived){
+        this.fsp.writeArrivalTime(now);
+        this.fsp.decideCheckInTime(now);
+        this.hasArrived = true;
+      }
+      
+      //Lagrer tidspunktet man ankom jobb
+
+      
+      if (this.sentNotification == false){
+        /*this.sendCustomizedNotification();*/
+        this.sentNotification =true;
+      }
+    }
     this.lastTimestamp = position.timestamp;
   });
  
@@ -99,40 +119,10 @@ this.watch = this.geolocation.watchPosition(options).filter((p: any) => p.code =
       console.log("ER VI PÅ JOBB?????")
       console.log(booleanPointInPolygon(pt,poly));
       this.paJobb = booleanPointInPolygon(pt,poly);
-      this.evaluateSendNotification()
       if(this.paJobb == false){
-        this.sentNotification == false;
+        this.sentNotification = false;
+        this.hasArrived = false;
       }
   }
-
-  evaluateSendNotification(){
-    //Må her sjekke om man er satt opp til å arbeide i nærmeste periode. 
-
-    //Hent data fra firebase på om man skal jobbe denne dagen og i nærmeste periode. sendArrivalNotification slår bare til dersom man snart skal jobbe eller har begynt. 
-    
-    console.log('Her kommer logg! \n');
-    console.log(this.paJobb)
-    console.log(this.sentNotification)
-    if(this.paJobb && this.sentNotification == false){
-      this.notifications.sendArrivalNotification();
-      this.sentNotification = true;
-
-      /* Knows that you have arrived at work, so will also schedule notification for lunch */
-      this.notifications.scheduleLunchNotification();
-      this.onLocationTime = new Date();
-    }
-
-  }
-
-  //Skal sjekke om du har en økt, sånn at du blir sjekket inn. 
-  canCheckIn(timeStamp : any) {
-    var dayStart = new Date(timeStamp);
-    var currentDate = new Date();
-    if (dayStart.getMonth() == currentDate.getMonth() && dayStart.getDate() == currentDate.getDate() || currentDate.getDate() - dayStart.getDate() > 0 ){
-      return true;
-    }
-    return false;
-  }
-
 
 }
