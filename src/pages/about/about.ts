191a02dde85885d4098054f7b25f72f0d7308fe3
Bjarte Espedokken <bjarteespedokken@Bjartes-MacBook-Pro.local>
2018-08-02 16:21:05 +0200

    lagt til statistikkstide

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -7,8 +7,30 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
+  public toggle1 = false;
+  public toggle2 = false;
+  
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  toggle(){
+    if (this.toggle1) {
+      this.toggle1 = false;
+    }
+    else {
+      this.toggle1 = true;
+    }
+  }
+
+  toggle21(){
+    if (this.toggle2) {
+      this.toggle2 = false;
+    }
+    else {
+      this.toggle2 = true;
+    }
   }
 
 }
